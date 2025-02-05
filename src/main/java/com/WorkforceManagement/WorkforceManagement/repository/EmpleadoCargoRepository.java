e9608efcaec133aa46790a3291411d3d030450af
LilyVaz <vasquezlily75@gmail.com>
2025-02-05 12:47:19 -0600

    Clase repository EmpleadoCargo

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/repository/EmpleadoCargoRepository.java
@@ -0,0 +1,10 @@
+package com.WorkforceManagement.WorkforceManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.WorkforceManagement.WorkforceManagement.model.EmpleadoCargo;
+
+public interface EmpleadoCargoRepository extends JpaRepository<EmpleadoCargo, Integer>{
+
+    
+}
