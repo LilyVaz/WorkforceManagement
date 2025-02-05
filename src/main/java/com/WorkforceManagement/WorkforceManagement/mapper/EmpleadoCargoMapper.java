45987dc2b43ba5565d98a73da9d88e38db3ed660
LilyVaz <vasquezlily75@gmail.com>
2025-02-05 12:39:00 -0600

    Clase mapper  EmpleadoCargo

--- src/main/java/com/WorkforceManagement/WorkforceManagement/mapper/EmpleadoCargoMapper.java
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/mapper/EmpleadoCargoMapper.java
@@ -2,7 +2,11 @@ package com.WorkforceManagement.WorkforceManagement.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.WorkforceManagement.WorkforceManagement.dto.EmpleadoCargoDTO;
+import com.WorkforceManagement.WorkforceManagement.model.EmpleadoCargo;
+
 @Mapper(componentModel = "spring")
 public interface EmpleadoCargoMapper {
-    
+    EmpleadoCargoDTO toDTO(EmpleadoCargo empleadoCargo);
+    EmpleadoCargo toEntity(EmpleadoCargoDTO empleadoCargoDTO);
 }
