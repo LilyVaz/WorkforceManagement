7ca5a0ab1b48a5a730832cbfa6cd581a31e988e7
LilyVaz <vasquezlily75@gmail.com>
2025-02-05 12:36:43 -0600

    Clase DTO  EmpleadoCargo

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/dto/EmpleadoCargoDTO.java
@@ -0,0 +1,9 @@
+package com.WorkforceManagement.WorkforceManagement.dto;
+
+import lombok.Data;
+
+@Data
+public class EmpleadoCargoDTO {
+    private Integer idEmpleadoCargo;
+    private String EmpleadoCargo;
+}
