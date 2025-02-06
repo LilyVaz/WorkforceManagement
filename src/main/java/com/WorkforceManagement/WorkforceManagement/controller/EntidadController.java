317055473ac30ed924420ead914c90b4c7f08b9d
LilyVaz <vasquezlily75@gmail.com>
2025-02-06 09:26:05 -0600

    Controller Entidad

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/controller/EntidadController.java
@@ -0,0 +1,35 @@
+package com.WorkforceManagement.WorkforceManagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.WorkforceManagement.WorkforceManagement.dto.EntidadDTO;
+import com.WorkforceManagement.WorkforceManagement.model.Entidad;
+import com.WorkforceManagement.WorkforceManagement.service.EntidadService;
+import com.WorkforceManagement.WorkforceManagement.service.GenericService;
+
+@RestController
+@RequestMapping("/api/entidades")
+public class EntidadController extends GenericController<Entidad, Integer>{
+    
+    private final EntidadService entidadService;
+
+    public EntidadController(EntidadService entidadService){
+        this.entidadService=entidadService;
+    }
+
+    @Override
+    protected GenericService<Entidad, Integer>getService(){
+        return entidadService;
+    }
+
+    @PutMapping("/{idEntidad}")
+    public ResponseEntity<EntidadDTO>update(@PathVariable Integer idEntidad, @RequestBody EntidadDTO entidadDTO){
+        EntidadDTO updateEntidad= entidadService.update(idEntidad, entidadDTO);
+        return updateEntidad!=null?ResponseEntity.ok(updateEntidad): ResponseEntity.notFound().build();
+    }
+}
