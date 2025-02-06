44df2cb431cb7fb56069662dacc8ba24114507bc
LilyVaz <vasquezlily75@gmail.com>
2025-02-06 07:11:31 -0600

    Entity Rubros

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/model/Rubro.java
@@ -0,0 +1,20 @@
+package com.WorkforceManagement.WorkforceManagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Rubros")
+public class Rubro {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer idRubro;
+    
+    private String rubro;
+}
