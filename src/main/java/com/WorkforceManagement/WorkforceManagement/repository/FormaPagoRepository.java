699b10d9a0d5c1f4798c9a80ff14e94697b96203
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 09:00:42 -0600

    Repository FormaPago

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/repository/FormaPagoRepository.java
@@ -0,0 +1,9 @@
+package com.WorkforceManagement.WorkforceManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.WorkforceManagement.WorkforceManagement.model.FormaPago;
+
+public interface FormaPagoRepository extends JpaRepository<FormaPago, Integer>{
+
+}
