b45edeecb34a824b9618c69301b25d0d266d6258
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 14:04:17 -0600

    Repository TipoContrato

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/repository/TipoContrato.java
@@ -0,0 +1,7 @@
+package com.WorkforceManagement.WorkforceManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoContrato extends JpaRepository<TipoContrato, Integer>{
+    
+}
