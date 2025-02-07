15464618af2b36d382b172ea7dfc3d71f763ccd5
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 14:12:07 -0600

    Repository CatDepartamento

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/repository/CatDepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.WorkforceManagement.WorkforceManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.WorkforceManagement.WorkforceManagement.model.CatDepartamento;
+
+public interface CatDepartamentoRepository extends JpaRepository<CatDepartamento, Integer>{
+    
+}
