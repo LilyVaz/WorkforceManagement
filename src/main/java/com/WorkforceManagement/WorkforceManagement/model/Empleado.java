80af1ac9c4c2bd0d409733ab7433700479325327
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 09:46:20 -0600

    entity empleado

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/model/Empleado.java
@@ -0,0 +1,40 @@
+package com.WorkforceManagement.WorkforceManagement.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "empleados")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id_empleado")
+    private Integer idEmpleado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_persona", nullable = false)
+    private Integer persona;
+
+    private Integer codigoEmpleado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_formapago", nullable = false)
+    private Integer formaPago;
+
+    private Integer numIss;
+
+    @ManyToOne
+    @JoinColumn(name = "afp", nullable = false)
+    private Integer afp;
+
+    private Integer numNup;
+
+}
