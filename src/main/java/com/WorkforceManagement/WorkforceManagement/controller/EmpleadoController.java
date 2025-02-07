c2a6be404c9fb96bf246417bb275335315135898
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 12:56:12 -0600

    Controller empleado

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/controller/EmpleadoController.java
@@ -0,0 +1,40 @@
+package com.WorkforceManagement.WorkforceManagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.WorkforceManagement.WorkforceManagement.dto.EmpleadoDTO;
+import com.WorkforceManagement.WorkforceManagement.model.Empleado;
+import com.WorkforceManagement.WorkforceManagement.service.EmpleadoService;
+import com.WorkforceManagement.WorkforceManagement.service.GenericService;
+
+@RestController
+@RequestMapping("/api/empleados")
+public class EmpleadoController extends GenericController<Empleado, Integer>{
+    private final EmpleadoService empleadoService;
+
+    public EmpleadoController(EmpleadoService empleadoService){
+        this.empleadoService=empleadoService;
+    }
+
+    @Override
+    protected GenericService<Empleado, Integer>getService(){
+        return empleadoService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<EmpleadoDTO> saveEmpleado(@RequestBody EmpleadoDTO empleadoDTO){
+        return ResponseEntity.ok(empleadoService.saveEmpleado(empleadoDTO));
+    }
+
+    @PutMapping("/update/{idEmpleado}")
+    public ResponseEntity<EmpleadoDTO>update(@PathVariable Integer idEmpleado, @RequestBody EmpleadoDTO empleadoDTO){
+        EmpleadoDTO updateEmpleado=empleadoService.updateEmpleado(idEmpleado, empleadoDTO);
+        return updateEmpleado!=null?ResponseEntity.ok(updateEmpleado):ResponseEntity.notFound().build();
+    }
+}
