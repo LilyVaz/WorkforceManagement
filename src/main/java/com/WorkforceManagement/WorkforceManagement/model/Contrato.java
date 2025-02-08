8ef724aab734934b8f62c209832fc058830ee38c
LilyVaz <vasquezlily75@gmail.com>
2025-02-07 23:26:37 -0600

    Entity Contrato

---
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/model/Contrato.java
@@ -0,0 +1,46 @@
+package com.WorkforceManagement.WorkforceManagement.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "contratos")
+public class Contrato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_contrato")
+    private Integer idContrato;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_empleado", nullable = false)
+    private Empleado idEmpleado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo_contrato", nullable = false)
+    private TipoContrato idTipoContrato;
+
+    @ManyToOne
+    @JoinColumn(name = "id_empleado_cargo")
+    private EmpleadoCargo idEmpleadoCargo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cat_departamento")
+    private CatDepartamento idCatDepartamento;
+
+    private Date fechaInicio;
+
+    private Double salario;
+
+    private Date fechaFin;
+
+}
