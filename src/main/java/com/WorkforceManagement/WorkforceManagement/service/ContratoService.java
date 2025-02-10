50a818ab3830ab8bc1ef0ef4e4c64c60f7f86a5a
LilyVaz <vasquezlily75@gmail.com>
2025-02-09 19:30:08 -0600

    Metodo save generico

--- src/main/java/com/WorkforceManagement/WorkforceManagement/service/ContratoService.java
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/service/ContratoService.java
@@ -1,21 +1,20 @@
 package com.WorkforceManagement.WorkforceManagement.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.WorkforceManagement.WorkforceManagement.dto.ContratoDTO;
 import com.WorkforceManagement.WorkforceManagement.mapper.ContratoMapper;
-import com.WorkforceManagement.WorkforceManagement.model.CatDepartamento;
 import com.WorkforceManagement.WorkforceManagement.model.Contrato;
-import com.WorkforceManagement.WorkforceManagement.model.Empleado;
-import com.WorkforceManagement.WorkforceManagement.model.EmpleadoCargo;
-import com.WorkforceManagement.WorkforceManagement.model.TipoContrato;
 import com.WorkforceManagement.WorkforceManagement.repository.CatDepartamentoRepository;
 import com.WorkforceManagement.WorkforceManagement.repository.ContratoRepository;
 import com.WorkforceManagement.WorkforceManagement.repository.EmpleadoCargoRepository;
 import com.WorkforceManagement.WorkforceManagement.repository.EmpleadoRepository;
 import com.WorkforceManagement.WorkforceManagement.repository.TipoContratoRepository;
+import com.WorkforceManagement.WorkforceManagement.service.util.RelationData;
 
 @Service
 public class ContratoService extends GenericService<Contrato, Integer>{
@@ -37,7 +36,7 @@ public class ContratoService extends GenericService<Contrato, Integer>{
         return contratoRepository;
     }
 
-    public ContratoDTO saveContrato(ContratoDTO contratoDTO){
+    /*public ContratoDTO saveContrato(ContratoDTO contratoDTO){
         Empleado empleado=empleadoRepository.findById(contratoDTO.getEmpleado())
             .orElseThrow(()-> new RuntimeException("Empleado no encontrado"));
         
@@ -59,5 +58,20 @@ public class ContratoService extends GenericService<Contrato, Integer>{
         Contrato saveContrato=contratoRepository.save(contrato);
 
         return contratoMapper.toDTO(saveContrato);
+    }*/
+
+    public ContratoDTO saveContrato(ContratoDTO contratoDTO) {
+    Contrato contrato = contratoMapper.toEntity(contratoDTO);
+
+    Map<String, RelationData<?, ?>> foreignKeys = Map.of(
+        "empleado", new RelationData<>(empleadoRepository, contratoDTO.getEmpleado()),
+        "tipoContrato", new RelationData<>(tipoContratoRepository, contratoDTO.getTipoContrato()),
+        "empleadoCargo", new RelationData<>(empleadoCargoRepository, contratoDTO.getEmpleadoCargo()),
+        "catDepartamento", new RelationData<>(catDepartamentoRepository, contratoDTO.getCatDepartamento())
+    );
+
+    Contrato savedContrato = saveWithRelations(contrato, foreignKeys);
+    return contratoMapper.toDTO(savedContrato);
     }
+
 }
