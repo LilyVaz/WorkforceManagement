44a1ce4d56fe3856358929356a41ab56251f4cbd
LilyVaz <vasquezlily75@gmail.com>
2025-02-09 20:10:08 -0600

    metodo generico para guardar registros con relaciones

--- src/main/java/com/WorkforceManagement/WorkforceManagement/controller/GenericController.java
+++ src/main/java/com/WorkforceManagement/WorkforceManagement/controller/GenericController.java
@@ -1,14 +1,17 @@
 package com.WorkforceManagement.WorkforceManagement.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.WorkforceManagement.WorkforceManagement.service.GenericService;
+import com.WorkforceManagement.WorkforceManagement.service.util.RelationData;
 
 public abstract class GenericController<T, ID> {
     protected abstract GenericService<T, ID> getService();
@@ -27,6 +30,13 @@ public abstract class GenericController<T, ID> {
     public T save(@RequestBody T entity){
         return getService().save(entity);
     }
+
+    @PostMapping("/relations")
+    public T saveRelations(
+        @RequestBody T entity,
+        @RequestParam Map<String, RelationData<?,?>>foreignKeys){
+            return getService().saveWithRelations(entity, foreignKeys);
+        }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable ID id){
